79a2c7f0ebacda9943f0844877013db5bc4caf7f
Andrew Wang <andrewwang39@gmail.com>
2018-09-28 21:20:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(arr)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
+    arr.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
     end
     puts current_line
